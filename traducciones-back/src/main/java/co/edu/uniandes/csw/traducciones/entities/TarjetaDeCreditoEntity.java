26fb409dfbaf2cae7b6cfb642045d78a8eb0bf72
SIS\ra.forero11 <ra.forero11@uniandes.edu.co>
2017-09-24 22:28:00 -0500

    Cambio orden carpetas para sonar y jenkins

---
+++ traducciones-back/src/main/java/co/edu/uniandes/csw/traducciones/entities/TarjetaDeCreditoEntity.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.traducciones.entities;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author ne.ortega
+ */
+@Entity
+public class TarjetaDeCreditoEntity extends BaseEntity {
+    
+    private int numero, codigoSeguridad;
+    private String compañia, nombres, apellidos;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCodigoSeguridad() {
+        return codigoSeguridad;
+    }
+
+    public void setCodigoSeguridad(int codigoSeguridad) {
+        this.codigoSeguridad = codigoSeguridad;
+    }
+
+    public String getCompañia() {
+        return compañia;
+    }
+
+    public void setCompañia(String compañia) {
+        this.compañia = compañia;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
+}
