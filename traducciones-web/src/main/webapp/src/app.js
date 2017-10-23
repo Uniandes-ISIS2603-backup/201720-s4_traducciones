d52ecf2c66972703509d0be923b08d8db2ba85ca
ANYELLA VALERIA PEREZ BUENDIA <av.perezb@sis.virtual.uniandes.edu.co>
2017-10-23 13:01:37 -0500

    index

---
+++ traducciones-web/src/main/webapp/src/app.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng) {
+    var app = angular.module('mainApp', [
+        // External dependencies
+        'ui.router',
+       
+        // Internal modules dependencies       
+        'ofertasModule',
+        
+
+    ]);
+    // Resuelve problemas de las promesas
+    app.config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+        }]);
+})(window.angular);
+
+
+
---
+++ traducciones-web/src/main/webapp/src/modules/ofertas/ofertas.mod.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function (ng) {
+var mod = ng.module("ofertasModule", []);
+    mod.constant("ofertasContext", "api/ofertas");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/ofertas/';
+            $urlRouterProvider.otherwise("/ofertasList");
+
+            $stateProvider.state('ofertasList', {
+                url: '/ofertas',
+                views: {
+                    'mainView': {
+                        controller: 'ofertasCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'ofertas.list.html'
+                    }
+                }
+            });
+        }]);
+
+})(window.angular);
+
+
