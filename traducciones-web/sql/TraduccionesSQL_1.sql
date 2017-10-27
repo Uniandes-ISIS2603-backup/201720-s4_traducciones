1fbdd6c9fce0cf3caad0021b626b50e2233feda4
SIS\ra.forero11 <ra.forero11@uniandes.edu.co>
2017-10-27 16:39:31 -0500

    Target cambio

--- traducciones-web/sql/TraduccionesSQL_1.sql
+++ traducciones-web/sql/TraduccionesSQL_1.sql
@@ -1,4 +1,4 @@
-delete from TrabajoEntity;
 insert into TrabajoEntity (id, name, terminado) values (50, 'Ethelda', 0);
+insert into CalificacionEntity (id, name, comentario, valor,trabajo_id) values (50, 'Damian', 'Turpis.mp3', 84.5,50);
 
 
