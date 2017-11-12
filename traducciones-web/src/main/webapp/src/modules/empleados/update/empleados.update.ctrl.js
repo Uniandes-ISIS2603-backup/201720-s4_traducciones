214a0ec3e4d9221db00fd76eb9ca0365bd015945
JUAN CARLOS GLORIA CARBONELL <jc.gloria@sis.virtual.uniandes.edu.co>
2017-11-12 11:36:44 -0500

    cambios de tipo de empleado

--- traducciones-web/src/main/webapp/src/modules/empleados/update/empleados.update.ctrl.js
+++ traducciones-web/src/main/webapp/src/modules/empleados/update/empleados.update.ctrl.js
@@ -17,7 +17,14 @@
                 $state.go('empleadosList', {empleadoId: response.data.id}, {reload: true});
             });
         };
-    }
+            var temp = $scope.empleadoTipo;
+            var mySelect = document.getElementById('tipo');
+            for(var i = 0; i < mySelect.options.length; i++) {
+            if(i.value == temp) {
+            mySelect.selectedIndex = i;
+            break;
+  }
+    }}
 ]);
 }
     )(angular);
