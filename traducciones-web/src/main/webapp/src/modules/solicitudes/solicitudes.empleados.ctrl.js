6786af2cf4d3cb7e40779121b3e5f2440283852f
JUAN CARLOS GLORIA CARBONELL <jc.gloria@sis.virtual.uniandes.edu.co>
2017-11-29 00:51:33 -0500

    solicitudes

---
+++ traducciones-web/src/main/webapp/src/modules/solicitudes/solicitudes.empleados.ctrl.js
@@ -0,0 +1,29 @@
+(function (ng) {
+    var mod = ng.module("empleadoModule");
+    mod.constant("empleadoContext", "api/empleados");
+    mod.controller('empleadoCtrl', ['$scope', '$http', 'empleadosContext', '$state',
+        function ($scope, $http, empleadosContext, $state) {
+            $http.get(empleadosContext).then(function (response) {
+                $scope.empleadosRecords = response.data;
+            });
+
+            if ($state.params.empleadoId !== undefined) {
+                $http.get(empleadosContext + '/' + $state.params.empleadoId).then(function (response) {
+                    $scope.currentEmpleado = response.data;
+                });
+            }
+            $scope.darTipo = function darTipo(a){
+                if(a === 1){
+                    return 'Traductor';
+                }
+                if(a === 2){
+                    return "Corrector";
+                }
+                if(a === 3){
+                    return "Ambos";
+                }
+            }
+        }
+    ]);
+}
+)(angular);
